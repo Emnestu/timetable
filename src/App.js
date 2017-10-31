86e812487a23094350a190373a94d3ddbca51ff0
Alex Halenko <alexhalenko1337@hotmail.com>
2017-10-31 15:08:02 +0000

    Added support for both directions (to and from a place), added background, code refactoring

--- src/App.js
+++ src/App.js
@@ -12,9 +12,26 @@ class App extends Component {
     super(props)
 
     this.state = {
-      data: []
+      data1: [],
+      data2: [],
+      busRouteNumber: '',
+      timeTableHeadings: ['Please enter a route number','Please enter a route number']
     };
+
+    // For entering bus stop numbers: https://reactjs.org/docs/forms.html
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({ busRouteNumber: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.makeCORSRequest();
+    event.preventDefault();
+  }
+
   createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
@@ -41,7 +58,7 @@ class App extends Component {
 
   makeCORSRequest() {
     // var url = 'https://www.dublinbus.ie/Your-Journey1/Timetables/All-Timetables/66/';
-    var url = 'https://cors-anywhere.herokuapp.com/https://www.dublinbus.ie/Your-Journey1/Timetables/All-Timetables/67/';
+    var url = `https://cors-anywhere.herokuapp.com/https://www.dublinbus.ie/Your-Journey1/Timetables/All-Timetables/${this.state.busRouteNumber}/`;
     var xhr = this.createCORSRequest('GET', url);
     if (!xhr) {
       throw new Error('CORS not supported');
@@ -49,69 +66,69 @@ class App extends Component {
     xhr.onload = function () {
       var responseText = xhr.responseText;
 
-      //test
       var el = document.createElement('html');
       el.innerHTML = responseText;
+
+      // HEADINGS
+      var headingsElement = el.getElementsByClassName('TT_Title_left');
+      var allHeadings = [];
+
+      for (var i = 0; i < headingsElement.length; i++) {
+        allHeadings[i] = headingsElement[i].innerText.replace(/(\r\n|\n|\r)/gm, "").trim();
+      }
+
+      this.setState({ timeTableHeadings: allHeadings });
+
+      // TIMES
       var timesElement = el.getElementsByClassName('time');
       var allTimes = [];
-      var weekdayTimes = [], saturdayTimes = [], sundayTimes = [];
+      var weekdayTimes1 = [], saturdayTimes1 = [], sundayTimes1 = [], weekdayTimes2 = [], saturdayTimes2 = [], sundayTimes2 = [];
 
       for (var i = 0; i < timesElement.length; i++) {
         allTimes[i] = timesElement[i].innerText.trim();
       }
+
       var counter = 0;
       var currentCounter = 0;
 
-      for (var i = counter; i < allTimes.length - 1; i++) {
-        if (parseInt(allTimes[i + 1]) >= parseInt(allTimes[i])) {
-          weekdayTimes[i - currentCounter] = allTimes[i];
-        } else {
-          weekdayTimes[i - currentCounter] = allTimes[i];
-          counter = i + 1;
-          break;
+      var fillTimesArray = function(arrayName) {
+        for (var i = counter; i < allTimes.length - 1; i++) {
+          if (parseInt(allTimes[i + 1]) >= parseInt(allTimes[i])) {
+            arrayName[i - currentCounter] = allTimes[i];
+          } else {
+            arrayName[i - currentCounter] = allTimes[i];
+            counter = i + 1;
+            break;
+          }
         }
+        currentCounter = counter
       }
 
-      currentCounter = counter;
-      console.log(currentCounter);
+      fillTimesArray(weekdayTimes1);
+      fillTimesArray(saturdayTimes1);
+      fillTimesArray(sundayTimes1);
 
-      for (var i = counter; i < allTimes.length - 1; i++) {
-        if (parseInt(allTimes[i + 1]) >= parseInt(allTimes[i])) {
-          saturdayTimes[i - currentCounter] = allTimes[i];
-        } else {
-          saturdayTimes[i - currentCounter] = allTimes[i];
-          counter = i + 1;
-          break;
-        }
-      }
+      fillTimesArray(weekdayTimes2);
+      fillTimesArray(saturdayTimes2);
+      fillTimesArray(sundayTimes2);
 
-      currentCounter = counter;
-      console.log(currentCounter);
+      var data1 = [];
 
-      for (var i = counter; i < allTimes.length - 1; i++) {
-        if (parseInt(allTimes[i + 1]) >= parseInt(allTimes[i])) {
-          sundayTimes[i - currentCounter] = allTimes[i];
-        } else {
-          sundayTimes[i - currentCounter] = allTimes[i];
-          counter = i + 1;
-          break;
-        }
-      }
-
-      var data = [];
-      
-      var longestArrayLength = Math.max(weekdayTimes.length, saturdayTimes.length, sundayTimes.length);
+      var longestArrayLength = Math.max(weekdayTimes1.length, saturdayTimes1.length, sundayTimes1.length, weekdayTimes2.length, saturdayTimes2.length, sundayTimes2.length);
       for (var i = 0; i < longestArrayLength; i++) {
-        data.push({
-          "weekdayTime": weekdayTimes[i],
-          "saturdayTime": saturdayTimes[i],
-          "sundayTime": sundayTimes[i]
+        data1.push({
+          "weekdayTime1": weekdayTimes1[i],
+          "saturdayTime1": saturdayTimes1[i],
+          "sundayTime1": sundayTimes1[i],
+          "weekdayTime2": weekdayTimes2[i],
+          "saturdayTime2": saturdayTimes2[i],
+          "sundayTime2": sundayTimes2[i]
         });
       }
 
-      this.setState({ data });
+      this.setState({ data1 });
 
-      console.dir(data);
+      console.dir(data1);
 
       // process the response.
     }.bind(this);
@@ -122,28 +139,64 @@ class App extends Component {
   }
 
   render() {
-    this.makeCORSRequest();
-    const columns = [{
+    const columns1 = [{
       id: 'weekdays',
       Header: 'Weekdays',
-      accessor: 'weekdayTime' // Custom value accessors!
+      accessor: 'weekdayTime1' // Custom value accessors!
     },
     {
       id: 'saturdays',
       Header: 'Saturdays',
-      accessor: 'saturdayTime' // String-based value accessors!
+      accessor: 'saturdayTime1' // String-based value accessors!
     },
     {
       id: 'sundays',
       Header: 'Sundays',
-      accessor: 'sundayTime' // String-based value accessors!
+      accessor: 'sundayTime1' // String-based value accessors!
+    }]
+    const columns2 = [{
+      id: 'weekdays',
+      Header: 'Weekdays',
+      accessor: 'weekdayTime2' // Custom value accessors!
+    },
+    {
+      id: 'saturdays',
+      Header: 'Saturdays',
+      accessor: 'saturdayTime2' // String-based value accessors!
+    },
+    {
+      id: 'sundays',
+      Header: 'Sundays',
+      accessor: 'sundayTime2' // String-based value accessors!
     }]
     return (
       <div>
-        <ReactTable
-          data={this.state.data}
-          columns={columns}
-        />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Route Number:
+          <input type="text" value={this.state.busRouteNumber} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Go" />
+        </form>
+
+        <div id="timetable-container">
+          <div class="timetable">
+            <div class="center"><strong>{this.state.timeTableHeadings[0]}</strong></div>
+            <ReactTable
+              data={this.state.data1}
+              columns={columns1}
+              sortable={false}
+            />
+          </div>
+          <div class="timetable">
+            <div class="center"><strong>{this.state.timeTableHeadings[1]}</strong></div>
+            <ReactTable
+              data={this.state.data1}
+              columns={columns2}
+              sortable={false}
+            />
+          </div>
+        </div>
       </div>
     );
   }
